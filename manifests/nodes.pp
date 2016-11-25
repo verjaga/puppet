535e8ead90ced4be3196de0063fb815bbd21c61f
verjaga <verjaga@gmail.com>
2016-11-25 10:33:09 +0000

    primer push puppetmater

---
+++ manifests/nodes.pp
@@ -0,0 +1,8 @@
+node 'testpuppet.home'{
+      include test
+ }
+
+node default{
+      include test
+ }
+
---
+++ manifests/site.pp
@@ -0,0 +1,71 @@
+import "nodes.pp"
+
+
+class test{
+
+
+# Actualizar los repositorios de paquetes
+exec { "apt-get update":
+    command => "/usr/bin/apt-get update"
+}
+
+# Instalación de Apache
+package { "apache2":
+    ensure => present,
+    require => Exec["apt-get update"]
+}
+
+package { "htop":
+    ensure => present,
+    require => Exec["apt-get update"]
+}
+
+
+
+# Arrancar el servicio de Apache
+service { "apache2":
+    ensure => running,
+    require => Package["apache2"]
+}
+
+
+
+# Lista de paquetes de PHP para instalar
+$packages = [
+    "php5",
+    "php5-cli",
+    "php5-mysql",
+    "php5-dev",
+    "php5-curl",
+    "php-apc",
+    "libapache2-mod-php5"
+]
+
+# Instalación de los paquetes de PHP
+package { $packages:
+    ensure => present,
+    require => Exec["apt-get update"],
+    notify => Service["apache2"]
+}
+
+# Instalación del servidor de MySQL
+package { "mysql-server":
+    ensure => present,
+    require => Exec["apt-get update"]
+}
+
+# Arrancar el servicio de MySQL
+service { "mysql":
+    ensure => running,
+    require => Package["mysql-server"]
+}
+
+exec { "set-mysql-root-password":
+    unless => "mysqladmin -u root -p1234 status",
+    command => "mysqladmin -u root password 1234",
+    require => Service["mysql"],
+    path => "/bin:/usr/bin",
+}
+
+
+}
